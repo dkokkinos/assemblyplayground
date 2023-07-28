e3776c8b4827eff981a412de29778323206a17f6
jim <dmt.kokkinos@gmail.com>
2023-07-28 17:24:03 +0300

    init

---
+++ AssemblyPlaygroundConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyPlaygroundConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
