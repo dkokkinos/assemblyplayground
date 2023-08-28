aed359f0f56dea2a9fb57e1bafc9dd73d85ab265
Jim <dmt.kokkinos@gmail.com>
2023-08-28 23:47:27 +0300

    changes

--- AssemblyReflectionConsoleApp/Program.cs
+++ AssemblyReflectionConsoleApp/Program.cs
@@ -15,10 +15,31 @@ namespace AssemblyReflectionConsoleApp
             var mathCoreAssembly = typeof(Calculator).Assembly; // Gets the assembly of the Math.Core library
             // Math.Core, Version=1.0.0.1, Culture=neutral, PublicKeyToken=9a6e2ec23b9a60ea
 
-            // For the assemblies to have PublicKeyToken they must be signed. This also gives them a strong name.
-
-
             var executingAssembly = Assembly.GetExecutingAssembly();
+
+            var fullName = mathAdvancedAssembly.FullName; // the fully qualified name Math.Advanced, Version=2.0.0.0, Culture=neutral, PublicKeyToken=9a6e2ec23b9a60ea
+            var assemblyName = mathAdvancedAssembly.GetName(); // The fully qualified as AssemblyName object.
+            var simpleName = assemblyName.Name; // Math.Advanced
+            var fullyQualifiedName = assemblyName.FullName; // Math.Advanced, Version=2.0.0.0, Culture=neutral, PublicKeyToken=9a6e2ec23b9a60ea
+            var version = assemblyName.Version; // 2.0.0.0
+            var cultureInfo = assemblyName.CultureInfo; // empty if not a satellite assembly.
+            var publicKey = assemblyName.GetPublicKey();
+            string publicKeyAsString = string.Empty;
+            for (int i = 0; i < publicKey.GetLength(0); i++)
+                publicKeyAsString += string.Format("{0:x2}", publicKey[i]);
+
+            var publicKeyToken = assemblyName.GetPublicKeyToken(); // 9a6e2ec23b9a60ea
+            string publicKeyTokenAsString = string.Empty; // 9a6e2ec23b9a60ea
+            for (int i = 0; i < publicKeyToken.GetLength(0); i++)
+                publicKeyTokenAsString += string.Format("{0:x2}", publicKeyToken[i]);
+
+            var location = mathAdvancedAssembly.Location; // The location of the Assembly file.
+
+            // This operation performs an assembly resolution because the location of the assembly must be found from its simple name
+            var loadedAssembly = Assembly.Load("Math.Advanced");
+
+            // Same as the previous operation, this time providing the full name of the assembly.
+            var loadedAssemblyWithFullName = Assembly.Load("Math.Advanced, Version=2.0.0.0, Culture=neutral, PublicKeyToken=9a6e2ec23b9a60ea");
 
         }
     }
