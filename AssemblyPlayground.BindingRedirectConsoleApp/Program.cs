0f155aa641c251949904a6ba6a39b347e46710c4
Jim . <dmt.kokkinos@gmail.com>
2023-09-16 16:42:07 +0300

    changes

---
+++ AssemblyPlayground.BindingRedirectConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdvancedMath;
+using Newtonsoft.Json;
+using System;
+
+namespace AssemblyPlayground.BindingRedirectConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdvancedCalculator calculator = new AdvancedCalculator(); // AdvancedCalculator components exists in Math.Advanced assembly.
+            var res = calculator.Eval("1+1"); // <- this line raises an exception because this causes the runtime to load the Newtosoft.Json v11.0.1 which
+            // is references by the Math.Advanced assembly. However, the application also references the Newtonsoft.Json assembly but v13.0.1, so the runtime
+            // cannot find the v11.0.1
+            var objResult = JsonConvert.DeserializeObject(res);
+            Console.WriteLine(objResult);
+
+            Console.ReadKey();
+        }
+    }
+}
--- Math.Advanced/AdvancedCalculator.cs
+++ Math.Advanced/AdvancedCalculator.cs
@@ -1,15 +1,17 @@
 ﻿
 using Math.Core;
+using Newtonsoft.Json;
 
 namespace AdvancedMath
 {
     public class AdvancedCalculator
     {
-        public object Eval(string expression)
+        public string Eval(string expression)
         {
             NCalc.Expression expr = new NCalc.Expression(expression);
             var res = expr.Evaluate();
-            return res;
+            var jsonResult = JsonConvert.SerializeObject(new { result = res });
+            return jsonResult;
         }
 
         public int Add(int x, int y)
